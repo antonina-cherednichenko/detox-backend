e7ee610bb14361b2a546fb8eba2d96e51e707236
Antonina Cherednichenko <antonina.cherednichenko@gmail.com>
2016-10-30 12:01:57 +0200

    Event more experiments with heroku

--- index.js
+++ index.js
@@ -1,32 +1,29 @@
 // Import required modules.
 var express = require('express');
 var stormpath = require('express-stormpath');
- 
+
 // Initialize our Express app.
 var app = express();
- 
+
 // Configure Stormpath.
 app.use(stormpath.init(app, {
-  application: {
-    href: process.env.STORMPATH_APPLICATION_HREF
-  },
-  website: true,
-  web: {
-    login: {
-      nextUri: '/dashboard'
+    expand: {
+        customData: true,
+    },
+    web: {
+        produces: ['application/json']
     }
-  }
 }));
- 
+
 // Generate a simple home page.
-app.get('/', function(req, res) {
-  res.send("Hey there! Thanks for visting the site! Be sure to <a href='/login'>login</a>!");
+app.get('/', function (req, res) {
+    res.send("Hey there! Thanks for visting the site! Be sure to <a href='/login'>login</a>!");
 });
- 
+
 // Generate a simple dashboard page.
-app.get('/dashboard', stormpath.loginRequired, function(req, res) {
-  res.send('Hi: ' + req.user.email + '. Logout <form action="/logout" method="POST"><button type="submit">Logout</button></form>');
+app.get('/dashboard', stormpath.loginRequired, function (req, res) {
+    res.send('Hi: ' + req.user.email + '. Logout <form action="/logout" method="POST"><button type="submit">Logout</button></form>');
 });
- 
+
 // Listen for incoming requests and serve them.
-app.listen(3000);
+app.listen(process.env.PORT || 3000);
