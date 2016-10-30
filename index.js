0e58f2df24b2f39bbe5fd34d3268c4348bce1e8f
Antonina Cherednichenko <antonina.cherednichenko@gmail.com>
2016-10-30 12:51:15 +0200

    Add retrieval of content

--- index.js
+++ index.js
@@ -1,6 +1,7 @@
 // Import required modules.
 var express = require('express');
 var stormpath = require('express-stormpath');
+var fs = require('fs');
 
 // Initialize our Express app.
 var app = express();
@@ -22,11 +23,14 @@ app.get('/', function (req, res) {
     res.send("Hey there! Thanks for visting the site! This is backend of Detox and Diets mobile app");
 });
 
+// Retrieving content for mobile app
+app.get('/data', function(req, res) {
+  res.json(fs.readFileSync('content/programs_data.json', 'utf-8'));
+});
 
-
-app.get('/data', stormpath.apiAuthenticationRequired, function(req, res) {
-  res.json({notes: "This is your notebook. Edit this to start saving your notes!"})
-})
+// app.get('/data', stormpath.apiAuthenticationRequired, function(req, res) {
+//   res.json({notes: "This is your notebook. Edit this to start saving your notes!"})
+// })
 
 // Listen for incoming requests and serve them.
 app.listen(process.env.PORT || 3000);
