16db7a5c5f4883f355d99acf23d7bca6c3dfe342
Antonina Cherednichenko <antonina.cherednichenko@gmail.com>
2016-11-25 16:55:46 +0200

    Add cross referncing

--- index.js
+++ index.js
@@ -18,6 +18,16 @@ var app = express();
 
 app.use(express.static('public'));
 
+var allowCrossDomain = function(req, res, next) {
+    res.header('Access-Control-Allow-Origin', '*');
+    res.header('Access-Control-Allow-Methods', 'GET,PUT,POST,DELETE');
+    res.header('Access-Control-Allow-Headers', 'Content-Type');
+
+    next();
+}
+
+app.use(allowCrossDomain);
+
 // Generate a simple home page.
 app.get('/', function (req, res) {
     res.send("Hey there! Thanks for visting the site! This is backend of Detox and Diets mobile app");
